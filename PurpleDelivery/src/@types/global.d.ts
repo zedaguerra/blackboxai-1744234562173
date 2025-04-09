87deeeda756aaa1a5ffcf687fc175b8e4a45906a
zedaguerra <134428352+zedaguerra@users.noreply.github.com>
2025-04-09 18:36:35 -0300

    Add PurpleDelivery/src/@types/global.d.ts 1744234595358

---
+++ PurpleDelivery/src/@types/global.d.ts
@@ -0,0 +1,9 @@
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.svg';
+
+export type RootStackParamList = {
+  Home: undefined;
+  // Add other screens here as needed
+};
